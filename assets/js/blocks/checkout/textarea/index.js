043fed896f71d59d30fd43e33072a47221f75394
Albert Juhé Lluveras <aljullu@gmail.com>
2019-06-06 15:58:05 +0200

    Capitalize Checkout Textarea
    Co-Authored-By: Joshua T Flowers <joshuatf@gmail.com>

--- assets/js/blocks/checkout/textarea/index.js
+++ assets/js/blocks/checkout/textarea/index.js
@@ -6,7 +6,7 @@ import { registerBlockType } from '@wordpress/blocks';
 import { TextareaControl } from '@wordpress/components';
 
 registerBlockType( 'woocommerce/checkout-textarea', {
-	title: __( 'Checkout textarea', 'woo-gutenberg-products-block' ),
+	title: __( 'Checkout Textarea', 'woo-gutenberg-products-block' ),
 	category: 'woocommerce-checkout',
 	keywords: [ __( 'WooCommerce', 'woo-gutenberg-products-block' ) ],
 	supports: {
